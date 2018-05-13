60a5a206e97af75b97e1eaa32c29dbd6eca31219
Nganie <ngan.tp.quan@gmail.com>
2018-05-13 05:40:18 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(love)
+  if block_given?
+  indigo = 22
+
+  while indigo < love.length
+    yield love[indigo]
+    indigo = love + 1
+  end
+  love
+else
+ love**2
+end
 end
